faedb7213db24476b2842fa73a577ff1ac34e1c6
devrose04 <webdevrose04@gmail.com>
2023-12-15 01:21:43 +0800

    lasted update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
    // $("#hamburger").click(function() {
@@ -6,17 +6,17 @@ $(document).ready(function() {
    //    $(".sidenav").toggleClass("active-nav");
    // })
 
-   $("#hamburger").click(function() {
+   $("#hamburger").click(function () {
       $(".sidenav").toggleClass("active-nav");
    })
 
-   $("#showsidenav").click(function() {
+   $("#showsidenav").click(function () {
       $(".sidenav").toggleClass("active-nav");
    })
 
-   
 
-   $(".nav-item").click(function(){
+
+   $(".nav-item").click(function () {
       // $(this).children(".dropdown-menus").toggleClass("'active-drop'");
       $(this).siblings(".fal").toggleClass('active-fal');
    });
@@ -26,11 +26,11 @@ $(document).ready(function() {
    function checkWidth() {
       var windowSize = $(window).width();
       if (windowSize > 974) {
-           $('#navigration .dropdown > .nav-link').removeClass('enabled').addClass('disabled');
-           $('#navigration .dropdown-2 > .dropdown-item').removeClass('enabled').addClass('disabled');
+         $('#navigration .dropdown > .nav-link').removeClass('enabled').addClass('disabled');
+         $('#navigration .dropdown-2 > .dropdown-item').removeClass('enabled').addClass('disabled');
       } else {
-           $('#navigration .dropdown > .nav-link').removeClass('disabled').addClass('enabled');
-           $('#navigration .dropdown-2 > .dropdown-item').removeClass('disabled').addClass('enabled');
+         $('#navigration .dropdown > .nav-link').removeClass('disabled').addClass('enabled');
+         $('#navigration .dropdown-2 > .dropdown-item').removeClass('disabled').addClass('enabled');
       }
    }
    // Execute on load
@@ -40,9 +40,9 @@ $(document).ready(function() {
 
 
 
-   $(window).scroll(function() {
+   $(window).scroll(function () {
       var st = $(this).scrollTop();
-      if( st > 10 ) {
+      if (st > 10) {
          $("#navigration").css({
             background: "#222429"
          });
@@ -62,18 +62,19 @@ $(document).ready(function() {
    });
 
 
+
    var owl = $('#homeSection .owl-carousel');
    owl.owlCarousel({
       animateOut: 'fadeOut',
-      loop:true,
-      nav:true,
-      margin:0,
-      autoplay:true,
-      autoplayTimeout:2000,
-      autoplayHoverPause:false,
-      responsive:{
-         0:{
-            items:1
+      loop: true,
+      nav: true,
+      margin: 0,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: false,
+      responsive: {
+         0: {
+            items: 1
          }
       }
    });
@@ -81,7 +82,7 @@ $(document).ready(function() {
 
    // PORTFOLIO SECTION
    var mixer = mixitup('#portfolio');
-   $(".indicator > span").click(function() {
+   $(".indicator > span").click(function () {
       $(".indicator > span").removeClass("active");
       $(this).addClass("active");
    });
@@ -101,18 +102,18 @@ $(document).ready(function() {
    var owl2 = $('#testimonialSection .owl-carousel');
    owl2.owlCarousel({
       animateOut: 'fadeOut',
-      loop:true,
-      nav:false,
+      loop: true,
+      nav: false,
       margin: 30,
-      autoplay:true,
-      autoplayTimeout:3000,
-      autoplayHoverPause:true,
-      responsive:{
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      responsive: {
          0: {
-            items:1
+            items: 1
          },
          768: {
-            items:2
+            items: 2
          }
       }
    });
@@ -144,14 +145,14 @@ $(document).ready(function() {
 
 
    var wow = new WOW({
-      boxClass:     'wow',      // animated element css class (default is wow)
+      boxClass: 'wow',      // animated element css class (default is wow)
       animateClass: 'animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       true,       // trigger animations on mobile devices (default is true)
-      live:         true,       // act on asynchronously loaded content (default is true)
-      callback:     function(box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
+      offset: 0,          // distance to the element when triggering the animation (default is 0)
+      mobile: true,       // trigger animations on mobile devices (default is true)
+      live: true,       // act on asynchronously loaded content (default is true)
+      callback: function (box) {
+         // the callback is fired every time an animation is started
+         // the argument that is passed in is the DOM node being animated
       },
       scrollContainer: null,    // optional scroll container selector, otherwise use window,
       resetAnimation: true,     // reset animation on end (default is true)
@@ -178,16 +179,16 @@ $(document).ready(function() {
 
    /*-- SCROLL UP --*/
    $(".scroll-up").fadeOut();
-   $(window).scroll(function() {
-      if ( $(this).scrollTop()>100 ) {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
          $(".scroll-up").fadeIn();
       } else {
          $(".scroll-up").fadeOut();
       };
    });
 
-   $(".scroll-up").click(function() {
-      $("html").animate({scrollTop: 0}, 1000);
+   $(".scroll-up").click(function () {
+      $("html").animate({ scrollTop: 0 }, 1000);
       return false;
    });
 
