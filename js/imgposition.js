c88819ae9ba57dfe00131bce0e55403629154613
devrose04 <webdevrose04@gmail.com>
2023-12-14 23:15:10 +0800

    '[updated]'

---
+++ js/imgposition.js
@@ -0,0 +1,28 @@
+// var element1 = document.getElementById('img001')
+// var element2 = document.getElementById('img002')
+// var element3 = document.getElementById('img003')
+
+// function onelementChange () {
+//   let temp = element3.className
+//   element3.className = element2.className
+//   element2.className = element1.className
+//   element1.className = temp
+// }
+
+// var startinterval = setInterval(onelementChange, 3000)
+
+// element1.addEventListener('click', () => {
+//   onelementChange()
+//   clearInterval(startinterval)
+//   startinterval = setInterval(onelementChange, 3000)
+// })
+// element2.addEventListener('click', () => {
+//   onelementChange()
+//   clearInterval(startinterval)
+//   startinterval = setInterval(onelementChange, 3000)
+// })
+// element3.addEventListener('click', () => {
+//   onelementChange()
+//   clearInterval(startinterval)
+//   startinterval = setInterval(onelementChange, 3000)
+// })
