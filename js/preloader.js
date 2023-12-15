58da21176df0f23aa25b30e67d531a5751e9ab33
devrose04 <webdevrose04@gmail.com>
2023-12-15 19:13:50 +0800

    add preloader

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Hide the preloader once the content is loaded
+    setTimeout(function () {
+       document.getElementById("preloader").style.display = "none";
+    }, 1000); // Adjust the delay as needed
+ });
