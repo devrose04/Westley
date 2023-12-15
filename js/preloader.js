df6ea3633063f92b737226ba31a93507591475a8
devrose04 <webdevrose04@gmail.com>
2023-12-15 22:30:34 +0800

    add property-lists

--- js/preloader.js
+++ js/preloader.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
     // Hide the preloader once the content is loaded
     setTimeout(function () {
        document.getElementById("preloader").style.display = "none";
-    }, 5000); // Adjust the delay as needed
+    }, 3000); // Adjust the delay as needed
  });
